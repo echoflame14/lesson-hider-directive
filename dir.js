c6fbada03cbeb76b909f9b6417a7a8fbfa95302c
josh <josh@joshs-MacBook-Air.local>
2016-07-19 14:39:49 -0600

    really frustrated with this lesson... commiting what I have for now, need to learn more before I continue

--- dir.js
+++ dir.js
@@ -3,12 +3,25 @@ angular.module("app").directive("dir", function(){
 		templateUrl: "lessonHider.html",
 		restrict: 'E',
 		scope:{
-
+			lesson: '='
 		},
-		link: function(scope,elements,attr){
-			console.log(scope);
-			console.log(elements);
-			console.log(attr);
+		controller: function($scope,srvc){
+			$scope.talkToJson = srvc.talkToJson();
+		},
+		link: function(scope,element,attr){
+			scope.talkToJson.then(function(response){
+				console.log(response);
+			scope.schedule = response.data;
+			for (var i = 0; i <	response.data.length; i++) {
+				console.log(i);
+				for (var x = 0; x < scope.lesson.length; x++) {
+					if(scope.lesson[x] === response.data[i].lesson){
+						console.log("taking out this one " + scope.lesson[x]);
+						element.css('text-decoration', 'line-through');
+					}
+				}
+			}
+			});
 		}
 	};
 });
--- srvc.js
+++ srvc.js
@@ -1,3 +1,6 @@
-angular.module("app").service("srvc", function(){
-	
+angular.module("app").service("srvc", function($http){
+	this.talkToJson = function(){
+		console.log("talking to json");
+		return $http.get('schedule.json');
+	};
 });
